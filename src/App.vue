178b686852f9fcc1b7624b8d247d0195477e5e46
Tuan N. Truong <t.truong@vehikl.com>
2019-04-14 16:40:01 -0400

    Create layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
 	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link>|
-			<router-link to="/exercises">exercises</router-link>
-			<router-link to="/profile">profile</router-link>
-			<router-link to="/calender">calender</router-link>
-			<router-link to="/goals">goals</router-link>
+		<div class="nav">
+			<router-link to="/">Home</router-link>
+			<router-link to="/exercises">Exercises</router-link>
+			<router-link to="/profile">Profile</router-link>
+			<router-link to="/calender">Calender</router-link>
+			<router-link to="/goals">Goals</router-link>
 		</div>
-		<router-view/>
+		<router-view class="routerView"/>
 	</div>
 </template>
 
 <style lang="scss">
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+	display: grid;
+	grid-template-areas: 'nav routerView';
+	grid-template-columns: 200px 1fr;
+	height: 100vh;
 }
-#nav {
-	padding: 30px;
+
+.nav {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+
 	a {
 		font-weight: bold;
 		color: #2c3e50;
+		text-decoration: none;
+		padding: 1rem 0;
+
 		&.router-link-exact-active {
 			color: #42b983;
 		}
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Reset from '@/assets/css/resets.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  Reset,
   render: h => h(App)
 }).$mount('#app')
